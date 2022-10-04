6cb44956cd2892ec29ab8b88beb125c99a0b668c
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-04 18:48:57 +0800

    scss

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,20 +1,22 @@
 // pages/blog/[id].js
 import { client } from "../../libs/client";
+import styles from '../../styles/Home.module.scss';
 
 export default function BlogId({ blog }) {
    return (
-      <main>
-         <h1>{blog.title}</h1>
-         <p>{blog.publishedAt}</p>
+      <main className={styles.main}>
+         <h1 className={styles.title}>{blog.title}</h1>
+         <p className={styles.publishedAt}>{blog.publishedAt}</p>
+         <p className="category">{blog.category && `${blog.category.name}`}</p>
          <div
             dangerouslySetInnerHTML={{
                __html: `${blog.body}`,
             }}
+            className={styles.post}
          />
       </main>
    );
 }
-
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
    const data = await client.get({ endpoint: "blog" });
