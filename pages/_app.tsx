ec632fc94716a3bd78743f74711fe274cf612ff2
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-06 11:17:43 +0800

    コメント

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps}) {
   return (
     <ChakraProvider>
       {/* Componentとは、仮引数　*/}
